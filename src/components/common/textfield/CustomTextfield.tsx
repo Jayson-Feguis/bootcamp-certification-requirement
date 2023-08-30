22b48f15a10611c986f53003b4b0962823664e43
Jayson-Feguis <jaysonfeguis121714@gmail.com>
2023-08-30 11:08:47 +0800

    fixed font color

--- src/components/common/textfield/CustomTextfield.tsx
+++ src/components/common/textfield/CustomTextfield.tsx
@@ -6,12 +6,12 @@ import { toSentenceCase } from "@/helpers/utils";
 const CustomTextfield: FC<InputBaseProps> = (props) => {
   return (
     <Box className="w-full">
-      <Typography variant="body1" className="text-white">
+      <Typography variant="body1" className="!text-white">
         {toSentenceCase(props?.name?.split(".")[0].replaceAll("_", " ") || "")}
       </Typography>
       <InputBase
         {...props}
-        className="text-white px-3 py-1 rounded-md w-full"
+        className="!text-white px-3 py-1 rounded-md w-full"
         sx={{ background: COLOR.PRIMARY }}
       />
     </Box>
