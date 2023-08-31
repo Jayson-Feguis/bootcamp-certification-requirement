12318f65d7d3ddf1f97eaafa97d98f6baed97362
Jayson-Feguis <jaysonfeguis121714@gmail.com>
2023-08-31 13:37:20 +0800

    remove delay

--- src/hooks/useLeaderboard.ts
+++ src/hooks/useLeaderboard.ts
@@ -90,7 +90,6 @@ function useLeaderboard() {
     if (program && publicKey) {
       try {
         setIsLoading(true);
-        await new Promise((r) => setTimeout(r, 2000));
         let leaderboardAccount: any =
           await program.account.leaderboardAccount.all(); // all([authorFilter(publicKey.toString())])
 
